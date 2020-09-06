ee4aeb80d48f4f9802fc1ed3bfd8206f2ae51598
WatcharapolxFa <69455513+WatcharapolxFa@users.noreply.github.com>
2020-09-06 16:15:02 +0700

    55

--- Separate_numbers/Source.cpp
+++ Separate_numbers/Source.cpp
@@ -1,9 +1,11 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<conio.h>
+
+// โปรแกรม รับค่า ตัวเลขแล้วแยกตัวเลขซ้ำออกมาไม่ซ้ำไม่ต้องแสดง
 int main()
 {
 
-
+	
 		char str[20];
 		printf("Message = ");
 		scanf_s("%s", str);
